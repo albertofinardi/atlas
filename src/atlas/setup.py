63b947041deb9b3c53bc50cc84262bfb22e17e44
Alberto Finardi <finara@usi.ch>
2025-05-16 17:21:14 +0200

    Renamed package, debug launch file

---
+++ src/atlas/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from glob import glob
+
+package_name = 'atlas'
+
+launch_files = glob('launch/*.launch')
+data_files =[
+    ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+]
+if launch_files:
+    data_files.append(('share/' + package_name + '/launch', glob('launch/*.launch')))
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='albertofinardi',
+    maintainer_email='finara@usi.ch',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'controller_node = atlas.controller_node:main',
+            'line_pid_node = atlas.line_pid_node:main',
+            'traffic_detection_node = atlas.traffic_detection_node:main'
+        ],
+    },
+)
